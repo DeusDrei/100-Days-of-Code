8af4f26c8b71b78ed3eb553500881d4cae3017e4
Alain Andrei I. Torno <82747749+DeusDrei@users.noreply.github.com>
2023-08-07 19:59:41 +0800

    Add files via upload

---
+++ Day 1 - band-name-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+ask = input("What's the name of the city you grew up in? ")
+ask1 = input("What is the name of your pet? ")
+print(f"Your band name could be {ask} {ask1}")
