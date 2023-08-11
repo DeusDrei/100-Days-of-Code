f1e29b1415286132c4663dcb54f798b33df7dc78
Alain Andrei I. Torno <82747749+DeusDrei@users.noreply.github.com>
2023-08-11 22:24:30 +0800

    Add files via upload

---
+++ Day 5/Exercise 2 - High Score.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+big = 0
+for i in student_scores:
+    if i > big:
+        big = i
+print(f"The highest score in the class is: {big}")
+
+
+
---
+++ Day 5/Exercise 3 - Adding Even Numbers.py
@@ -0,0 +1,6 @@
+#Write your code below this row 👇
+num = 0
+for i in range(101):
+    if i % 2 == 0:
+        num += i
+print(num)
