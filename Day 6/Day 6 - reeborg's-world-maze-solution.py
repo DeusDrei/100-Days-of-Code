40ea28a968b3efa251c1af2596db9aac677df3d4
Alain Andrei I. Torno <82747749+DeusDrei@users.noreply.github.com>
2023-08-12 22:07:28 +0800

    Add files via upload

---
+++ Day 6/Day 6 - reeborg's-world-maze-solution.py
@@ -0,0 +1,26 @@
+# def turn_right():
+#   turn_left()
+#   turn_left()
+#   turn_left()
+
+
+# def jump():
+#   if front_is_clear():
+#     move()
+#   if right_is_clear():
+#     turn_right()
+#   else:
+#     turn_left()
+
+
+# while not at_goal():
+#   if front_is_clear():
+#     move()
+#     if front_is_clear():
+#       jump()
+#     elif right_is_clear():
+#       turn_right()
+#   elif wall_on_right() and front_is_clear():
+#     move()
+#   elif wall_in_front():
+#     jump()
