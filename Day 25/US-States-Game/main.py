e812d0812f23c00622f1b3a62250e3be5f96bf5a
Alain Andrei I. Torno <82747749+DeusDrei@users.noreply.github.com>
2023-09-02 21:44:52 +0800

    Add files via upload

---
+++ Day 25/US-States-Game/main.py
@@ -0,0 +1,33 @@
+import turtle
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pd.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed = []
+
+while len(guessed) < 50:    
+    answer = screen.textinput(title=f"Guessed states {len(guessed)}/50", prompt="Enter a state: ").title()
+
+    if answer == "Exit":
+        missing_states = [state for state in all_states if state not in guessed]
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
+    if answer in all_states:
+        guessed.append(answer)
+        alain = turtle.Turtle()
+        alain.hideturtle()
+        alain.penup()
+        state_data = data[data.state == answer]
+        alain.goto(x=int(state_data.x), y=int(state_data.y))
+        alain.write(answer)
+
+
+
