114af70158187182b1fb06fa29db31712f63912a
Alain Andrei I. Torno <82747749+DeusDrei@users.noreply.github.com>
2023-09-16 20:35:47 +0800

    Add files via upload

---
+++ Day 32/Motivational-Quote-Sender/main.py
@@ -0,0 +1,22 @@
+import smtplib
+import random
+import datetime as dt
+
+my_email = "sample@gmail.com"
+my_password = "samplepassword"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+
+if weekday == 0:
+    with open("quotes.txt") as f:
+        quotes = [line.strip() for line in f.readlines()]
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr=my_email, 
+                            to_addrs=my_email, 
+                            msg=f"Subject:Monday Motivational Quote\n\n{random.choice(quotes)}")
+
+
