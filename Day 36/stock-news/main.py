c774db797ea578dcee45a34c28a5fceab39a3bab
Alain Andrei I. Torno <82747749+DeusDrei@users.noreply.github.com>
2023-09-21 17:43:15 +0800

    Add files via upload

---
+++ Day 36/stock-news/main.py
@@ -0,0 +1,66 @@
+import requests
+from twilio.rest import Client
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+APIKEY = "YMDLTLZS2WN7BOQW"
+NEWSAPIKEY = "f6f24cf0ec864ce78665d65a47a770b6"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+parameters = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": APIKEY,
+}
+response_stock = requests.get(STOCK_ENDPOINT, params=parameters)
+response_stock.raise_for_status()
+data = response_stock.json()
+
+closing_price = [value for key, value in data["Time Series (Daily)"].items()]
+
+yesterday = float(closing_price[0]["4. close"])
+day_before = float(closing_price[1]["4. close"])
+
+positive_difference = yesterday - day_before
+
+up_down = None
+if positive_difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+
+average = (yesterday + day_before) / 2
+percentage = round((positive_difference / average) * 100)
+
+if abs(percentage) > 1:
+    news_parameters = {
+        "q": COMPANY_NAME,
+        "apiKey": NEWSAPIKEY,
+    }
+    
+    response_news = requests.get(NEWS_ENDPOINT, params=news_parameters)
+    response_news.raise_for_status()
+    news_data = response_news.json()
+    articles = news_data["articles"][:3]
+
+    messages = []
+    for article in articles:
+        headline = article["title"]
+        brief = article["description"]
+        messages.append(f"{STOCK_NAME}: {up_down}{percentage}%\nHeadline: {headline}\nBrief: {brief}")
+
+    account_sid = 'AC978390d7ce17adb6f081de157e8e2f1f'
+    auth_token = '9255dc23a6f862ef1ac0c95cd59e8f6d'
+    client = Client(account_sid, auth_token)
+
+    for i in messages:
+        message = client.messages.create(
+            body=i,
+            from_='+17157695186',
+            to='+639913070670'
+        )
+ 
+
+
