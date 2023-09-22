621bcb76c0bc39d800dd0024aa30de2c082c7a65
Alain Andrei I. Torno <82747749+DeusDrei@users.noreply.github.com>
2023-09-22 21:41:40 +0800

    Add files via upload

---
+++ Day 37/habit-tracker/main.py
@@ -0,0 +1,57 @@
+import requests
+from datetime import datetime
+
+
+ID = "graph id"
+USERNAME = "your username"
+TOKEN = "your token"
+pixela_endpoint = "https://pixe.la/v1/users"
+
+parameters = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+# response = requests.post(url=pixela_endpoint, json=parameters)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": ID,
+    "name": "Study Time",
+    "unit": "hours",
+    "type": "float",
+    "color": "kuro"
+}
+
+header = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=header)
+# print(response.text)
+
+input_endpoint = f"{graph_endpoint}/{ID}"
+
+today = datetime.now()
+
+graph_input = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How many hours did you study today?: ")
+}
+
+response = requests.post(url=input_endpoint, json=graph_input, headers=header)
+print(response.text)
+
+delete_endpoint = f"{input_endpoint}/20230911"
+
+graph_update = {
+    "quantity": "1"
+}
+
+# response = requests.delete(url=delete_endpoint, headers=header)
+# print(response.text)
+
