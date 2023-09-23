79563f45d75bf475dc35549773bf024b4aa08232
Alain Andrei I. Torno <82747749+DeusDrei@users.noreply.github.com>
2023-09-23 20:30:54 +0800

    Add files via upload

---
+++ Day 38/workout-tracker/main.py
@@ -0,0 +1,38 @@
+import requests
+from datetime import datetime
+
+APP_ID = "app id"
+API_KEY = "api key"
+exercise_endpoint = "endpoint"
+gsheet_post_endpoint = "endpoint"
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY
+}
+
+parameters = {
+    "query": input("Tell me which exercises you did: ")
+}
+
+response_query = requests.post(url=exercise_endpoint, json=parameters, headers=headers)
+new_list = response_query.json()
+
+
+today = datetime.now()
+
+
+for i in new_list["exercises"]:
+
+    to_input = {
+        "workout": {
+            "date": today.strftime("%d/%m/%Y"),
+            "time": today.strftime("%X"),
+            "exercise": i["name"].title(),
+            "duration": i["duration_min"],
+            "calories": i["nf_calories"]
+        }
+    }
+
+    response_post = requests.post(gsheet_post_endpoint, json=to_input)
+    print(response_post.text)
