c73784a8204e286213b95104b52b3973541db6bc
Alain Andrei I. Torno <82747749+DeusDrei@users.noreply.github.com>
2024-02-05 18:59:58 +0800

    Update Day 15 - coffee-machine.py

--- Day 15/Day 15 - coffee-machine.py
+++ Day 15/Day 15 - coffee-machine.py
@@ -86,4 +86,4 @@ while is_on:
             payment = process_coins()
             if is_successful(payment, drink["cost"]):
                 make_coffee(choice, drink["ingredients"])
-                
+
